328452a9078fe878c720968886debb0a364a443f
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2024-03-10 20:09:57 +0100

    Generate all hashs

--- cmd/imagehash/main.go
+++ cmd/imagehash/main.go
@@ -5,11 +5,13 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"image"
 	"image/gif"
 	"image/jpeg"
 	"image/png"
 	"io"
 	"os"
+	"slices"
 	"strings"
 	"tux-lobload/store"
 
@@ -23,6 +25,18 @@ import (
 )
 
 var url = os.Getenv("POSTGRES_URL")
+var defaultHash = 1
+var hashes = []string{"averageHash", "perceptHash", "diffHash", "waveletHash"}
+var hashType = hashes[defaultHash]
+
+type hashData struct {
+	Checksumpicture string
+	Hash            uint64
+	Averagehash     uint64
+	PerceptionHash  uint64
+	DifferenceHash  uint64
+	Kind            byte
+}
 
 func init() {
 	level := zapcore.ErrorLevel
@@ -90,10 +104,18 @@ func main() {
 
 	flag.IntVar(&limit, "l", 50, "Maximum number of records loaded")
 	flag.StringVar(&preFilter, "f", "", "Prefix of title used in search")
+	flag.StringVar(&hashType, "h", hashes[defaultHash], "Hash type to use, valid are (averageHash,perceptHash,diffHash,waveletHash), default perceptHash")
 	flag.Parse()
+
+	fmt.Println("Start Bitgarten hash generator to find doublikates of pictures")
 
 	if preFilter != "" {
 		preFilter = fmt.Sprintf(" AND title LIKE '%s%%'", preFilter)
+	}
+
+	if !slices.Contains(hashes, hashType) {
+		fmt.Println("Incorrect hash parameter given:", hashType, "not in", hashes)
+		return
 	}
 
 	id, err := flynn.Handle(url)
@@ -101,6 +123,7 @@ func main() {
 		fmt.Println("POSTGRES error", err)
 		return
 	}
+	fmt.Println("Query database entries for one week not hashed")
 	query := &common.Query{
 		TableName:  "pictures",
 		Fields:     []string{"ChecksumPicture", "title", "mimetype", "media"},
@@ -109,34 +132,37 @@ func main() {
 		Search: "markdelete = false AND mimetype LIKE 'image/%'" + preFilter +
 			" and not exists(select 1 from picturehash ph where ph.checksumpicture = tn.checksumpicture and ph.updated_at < current_date + interval '1 week')",
 	}
+	counter := uint64(0)
+	processed := uint64(0)
 	_, err = id.Query(query, func(search *common.Query, result *common.Result) error {
+		counter++
 		p := result.Data.(*store.Pictures)
 		buffer := bytes.NewBuffer(p.Media)
-		var h *goimagehash.ImageHash
+		var hd *hashData
 		switch strings.ToLower(p.MIMEType) {
 		case "image/heic":
-			h, err = hashHeic(buffer)
+			hd, err = hashHeic(buffer)
 			if err != nil {
 				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
 				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
 				return nil
 			}
 		case "image/jpeg", "image/jpg":
-			h, err = hashJpeg(buffer)
+			hd, err = hashJpeg(buffer)
 			if err != nil {
 				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
 				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
 				return nil
 			}
 		case "image/png":
-			h, err = hashPng(buffer)
+			hd, err = hashPng(buffer)
 			if err != nil {
 				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
 				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
 				return nil
 			}
 		case "image/gif":
-			h, err = hashGif(buffer)
+			hd, err = hashGif(buffer)
 			if err != nil {
 				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
 				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
@@ -147,62 +173,104 @@ func main() {
 			log.Log.Errorf("Error unknown image format for %s/%s: %s\n", p.Title, p.ChecksumPicture, p.MIMEType)
 			return nil
 		}
-		fmt.Printf("%s -> %+v\n", p.Title, h)
-		insertHash(p, h)
+		hd.Checksumpicture = p.ChecksumPicture
+		hd.Hash = hd.PerceptionHash
+		fmt.Printf("%s -> %s\n", p.Title, hd.Checksumpicture)
+		err = insertHash(p, hd)
+		if err == nil {
+			processed++
+		}
 		return nil
 	})
 	if err != nil {
 		fmt.Println("Query error:", err)
 	}
+	fmt.Printf("Found %d pictures where %d pictures are hashed", counter, processed)
 	fmt.Println()
 }
 
-func insertHash(p *store.Pictures, h *goimagehash.ImageHash) error {
+func insertHash(p *store.Pictures, ph *hashData) error {
 	wid, err := flynn.Handle(url)
 	if err != nil {
 		fmt.Println("POSTGRES error", err)
 		return nil
 	}
 	insert := &common.Entries{
-		Fields: []string{"checksumpicture", "hash", "kind"},
-		Values: [][]any{{p.ChecksumPicture, h.GetHash(), byte(h.GetKind())}},
+		Fields:     []string{"checksumpicture", "hash", "averagehash", "perceptionHash", "differenceHash", "kind"},
+		DataStruct: ph,
+		// Values:     [][]any{{p.ChecksumPicture, h.GetHash(), byte(h.GetKind())}},
+		Values: [][]any{{ph}},
 	}
 	err = wid.Insert("picturehash", insert)
 	if err != nil {
-		fmt.Println("Error inserting :", err)
+		fmt.Printf("Error inserting %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
 		return err
 	}
 	return nil
 }
 
-func hashHeic(f io.Reader) (*goimagehash.ImageHash, error) {
+func hashHeic(f io.Reader) (*hashData, error) {
 	i, err := goheif.Decode(f)
 	if err != nil {
 		return nil, err
 	}
-	return goimagehash.PerceptionHash(i)
+	return generateHash(i)
 }
 
-func hashJpeg(f io.Reader) (*goimagehash.ImageHash, error) {
+func generateHash(i image.Image) (*hashData, error) {
+	ph := &hashData{}
+	h, err := hash(i, hashes[0])
+	if err != nil {
+		return nil, err
+	}
+	ph.Averagehash = h.GetHash()
+	h, err = hash(i, hashes[1])
+	if err != nil {
+		return nil, err
+	}
+	ph.PerceptionHash = h.GetHash()
+	h, err = hash(i, hashes[2])
+	if err != nil {
+		return nil, err
+	}
+	ph.DifferenceHash = h.GetHash()
+	return ph, nil
+}
+
+func hash(i image.Image, hType string) (*goimagehash.ImageHash, error) {
+	switch hType {
+	case hashes[0]:
+		return goimagehash.AverageHash(i)
+	case hashes[1]:
+		return goimagehash.PerceptionHash(i)
+	case hashes[2]:
+		return goimagehash.DifferenceHash(i)
+	case hashes[3]:
+		fmt.Println("Wavelet not yet support by system")
+	}
+	return nil, fmt.Errorf("unknown hashType")
+}
+
+func hashJpeg(f io.Reader) (*hashData, error) {
 	i, err := jpeg.Decode(f)
 	if err != nil {
 		return nil, err
 	}
-	return goimagehash.PerceptionHash(i)
+	return generateHash(i)
 }
 
-func hashPng(f io.Reader) (*goimagehash.ImageHash, error) {
+func hashPng(f io.Reader) (*hashData, error) {
 	i, err := png.Decode(f)
 	if err != nil {
 		return nil, err
 	}
-	return goimagehash.PerceptionHash(i)
+	return generateHash(i)
 }
 
-func hashGif(f io.Reader) (*goimagehash.ImageHash, error) {
+func hashGif(f io.Reader) (*hashData, error) {
 	i, err := gif.Decode(f)
 	if err != nil {
 		return nil, err
 	}
-	return goimagehash.PerceptionHash(i)
+	return generateHash(i)
 }
