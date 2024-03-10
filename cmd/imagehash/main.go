b81cd7ce13206f31153cc18245f90d50bcec06a7
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2024-03-10 12:08:55 +0100

    Add heic support

--- cmd/imagehash/main.go
+++ cmd/imagehash/main.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"image/gif"
 	"image/jpeg"
+	"image/png"
 	"io"
 	"os"
 	"tux-lobload/store"
@@ -13,6 +15,7 @@ import (
 	"github.com/corona10/goimagehash"
 	"github.com/tknie/flynn"
 	"github.com/tknie/flynn/common"
+	"github.com/tknie/goheif"
 	"github.com/tknie/log"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -35,6 +38,7 @@ func init() {
 		fmt.Println("Error initialize logging")
 		os.Exit(255)
 	}
+	os.Setenv("PGAPPNAME", "Bitgarten hash")
 }
 
 func initLogLevelWithFile(fileName string, level zapcore.Level) (err error) {
@@ -107,10 +111,39 @@ func main() {
 	_, err = id.Query(query, func(search *common.Query, result *common.Result) error {
 		p := result.Data.(*store.Pictures)
 		buffer := bytes.NewBuffer(p.Media)
-		h, err := hash(buffer)
-		if err != nil {
-			fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
-			log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
+		var h *goimagehash.ImageHash
+		switch p.MIMEType {
+		case "image/heic":
+			h, err = hashHeic(buffer)
+			if err != nil {
+				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
+				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
+				return nil
+			}
+		case "image/jpeg", "image/jpg":
+			h, err = hashJpeg(buffer)
+			if err != nil {
+				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
+				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
+				return nil
+			}
+		case "image/png":
+			h, err = hashPng(buffer)
+			if err != nil {
+				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
+				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
+				return nil
+			}
+		case "image/gif":
+			h, err = hashGif(buffer)
+			if err != nil {
+				fmt.Printf("Error generating hash for %s/%s: %v\n", p.Title, p.ChecksumPicture, err)
+				log.Log.Errorf("Error generating hash for %s/%s: %v", p.Title, p.ChecksumPicture, err)
+				return nil
+			}
+		default:
+			fmt.Printf("Error unknown image format for %s/%s: %s\n", p.Title, p.ChecksumPicture, p.MIMEType)
+			log.Log.Errorf("Error unknown image format for %s/%s: %s\n", p.Title, p.ChecksumPicture, p.MIMEType)
 			return nil
 		}
 		fmt.Printf("%s -> %+v\n", p.Title, h)
@@ -141,8 +174,32 @@ func insertHash(p *store.Pictures, h *goimagehash.ImageHash) error {
 	return nil
 }
 
-func hash(f io.Reader) (*goimagehash.ImageHash, error) {
+func hashHeic(f io.Reader) (*goimagehash.ImageHash, error) {
+	i, err := goheif.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	return goimagehash.PerceptionHash(i)
+}
+
+func hashJpeg(f io.Reader) (*goimagehash.ImageHash, error) {
 	i, err := jpeg.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	return goimagehash.PerceptionHash(i)
+}
+
+func hashPng(f io.Reader) (*goimagehash.ImageHash, error) {
+	i, err := png.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	return goimagehash.PerceptionHash(i)
+}
+
+func hashGif(f io.Reader) (*goimagehash.ImageHash, error) {
+	i, err := gif.Decode(f)
 	if err != nil {
 		return nil, err
 	}
